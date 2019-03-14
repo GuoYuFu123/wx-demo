598f0da01186f05196c6ff81caa5c169ac1db9c2
GuoYuFu123 <1872828785@qq.com>
2019-03-14 11:21:37 +0800

    update

--- pages/antV/antV.js
+++ pages/antV/antV.js
@@ -1,66 +1,52 @@
-// pages/antV/antV.js
+
+// index.js
+// import F2 from '@antv/wx-f2'; // 注：也可以不引入， initChart 方法已经将 F2 传入，如果需要引入，注意需要安装 @antv/wx-f2 依赖
+
+let chart = null;
+
+function initChart(canvas, width, height, F2) { // 使用 F2 绘制图表
+    const data = [
+        { year: '1951 年', sales: 38 },
+        { year: '1952 年', sales: 52 },
+        { year: '1956 年', sales: 61 },
+        { year: '1957 年', sales: 145 },
+        { year: '1958 年', sales: 48 },
+        { year: '1959 年', sales: 38 },
+        { year: '1960 年', sales: 38 },
+        { year: '1962 年', sales: 38 },
+    ];
+    chart = new F2.Chart({
+        el: canvas,
+        width,
+        height
+    });
+
+    chart.source(data, {
+        sales: {
+            tickCount: 5
+        }
+    });
+    chart.tooltip({
+        showItemMarker: false,
+        onShow(ev) {
+            const { items } = ev;
+            items[0].name = null;
+            items[0].name = items[0].title;
+            items[0].value = '¥ ' + items[0].value;
+        }
+    });
+    chart.interval().position('year*sales');
+    chart.render();
+    return chart;
+}
+
 Page({
+    data: {
+        opts: {
+            onInit: initChart
+        }
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+    onReady() {
+    }
+});
